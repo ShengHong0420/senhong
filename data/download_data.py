ab24a2f7f966d9cdeea32d5aa6ed57f5dc5f05a8
SblackOuO <rain6111213@gmail.com>
2025-05-16 14:26:06 +0800

    ignore this

--- data/download_data.py
+++ data/download_data.py
@@ -7,5 +7,5 @@ from datasets import load_dataset
 # 下載 test split
 # test_ds  = Food101(root="data/food101", split="test",  download=True)
 
-ds = load_dataset("tiennv/uecfood256", cache_dir="/data/hf_datasets")
-ds.save_to_disk("/data/uecfood256")
+# ds = load_dataset("tiennv/uecfood256", cache_dir="/data/hf_datasets")
+# ds.save_to_disk("/data/uecfood256")
